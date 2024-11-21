a1aa735434bd15dfcfd9efb1ad50b42c245b3bbb
bensagir <bsagir@baybridgedigital.com>
2024-11-21 18:44:28 +0200

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Link href="/about" style={{ backgroundColor: 'red' }}>
+        Go to About screen
+      </Link>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
